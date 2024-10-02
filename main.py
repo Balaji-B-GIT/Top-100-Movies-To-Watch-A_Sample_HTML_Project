fe636bd75151d2811f33cbf90889ec1f8ec8408f
Balaji B <balaji.balachandar1@gmail.com>
2024-10-02 18:16:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ response = requests.get(URL)
 
 empire_web_page = response.text
 soup = BeautifulSoup(empire_web_page, "lxml")
-title = soup.find_all(name="h3", class_="title")
-print(title)
+titles = soup.find_all(name="h3", class_="title")
 
 with open("./data/movies.txt", "w", encoding="utf-8") as file:
-    for movie in reversed(title):
+    for movie in reversed(titles):
         print(str(movie.getText()))
         file.write(str(movie.getText()))
         file.write("\n")
